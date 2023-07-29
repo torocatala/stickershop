c546fbf81fee1d3be91f08eed907e44345b1914d
Ivan Jimenez <torocatala@gmail.com>
2023-07-29 20:50:04 +0200

    Add a working but empty react for the frontend

--- run.sh
+++ run.sh
@@ -1,45 +1,39 @@
 #!/bin/bash
 reset
+
 # Check for Docker 
 if ! command -v docker &> /dev/null; then
   echo "Docker is required to run stickershop. Please install Docker and try again."
   exit 1
 fi
 
-# Check for Docker Compose
-if ! command -v docker-compose &> /dev/null; then
-  echo "Docker Compose is required to run stickershop. Please install Docker Compose and try again."
-  exit 1 
-fi
-
-# Stop the app
-echo "Stopping stickershop..."
-docker-compose down
-
 #Clean
+echo "Cleaning the environment..."
 docker rm -f $(docker ps -qa) 2>/dev/null
 docker volume rm $(docker volume ls -q) 2>/dev/null
-rm -f rbbackend/tmp/pids/server.pid
+rm -f rbbackend/tmp/pids/server.pid 2>/dev/null
+echo ""
 
 # Start the app
 echo "Starting stickershop..."
 docker-compose up -d --force-recreate
+echo ""
 
 # Check if the app started successfully 
-echo "Waiting for stickershop to start..."
-until curl -s http://localhost:3000; do
+echo "Waiting for stickershop sytem to start..."
+until curl -s http://localhost:3000 > /dev/null; do
   sleep 1 
 done
-echo "Stickershop started successfully at http://localhost:3000"
+echo "Stickershop frontend started successfully at http://localhost:3000"
+until curl -s http://localhost:3001 > /dev/null; do
+  sleep 1 
+done
+echo "Stickershop ruby backend started successfully at http://localhost:3001"
+echo ""
 
-# View logs
+# Useful info
 echo "To view logs, run:" 
 echo "  docker-compose logs -f"
-
-# Stop the app
-echo "To stop stickershop app, run:"
-echo "  docker-compose down"
+echo ""
 
 docker ps -a
-
-docker logs stickershop-rbbackend-1
