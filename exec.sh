9639640ab8fde924f26e520f4b6daa59e031910a
Ivan Jimenez <torocatala@gmail.com>
2023-07-29 21:14:55 +0200

    Better exec script

--- exec.sh
+++ exec.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-docker exec -ti stickershop-rbbackend-1 bash
+usage() {
+    echo "Invalid service name or no arguments supplied"
+    echo "Usage: ./docker_exec.sh {frontend|jsbackend|rbbackend}"
+    exit 1
+}
+
+if [ $# -eq 0 ]
+  then
+    usage
+fi
+
+service=$1
+
+if [ "$service" == "frontend" ] || [ "$service" == "jsbackend" ] || [ "$service" == "rbbackend" ]
+  then
+    container_id=$(docker-compose ps -q $service)
+    docker exec -it $container_id /bin/bash
+  else
+    usage
+fi
